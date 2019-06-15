a7b1cca10d9828a9545a87f03a7c0dd9c98886f3
Sunil Kumar <sunilkumarsheoran@gmail.com>
2019-06-15 10:16:31 -0700

    updated

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import argparse
 import pickle
 
-import torch
+import torch,os
+import numpy as np
 
 from torchmf import (BaseModule, BPRModule, BasePipeline,
                      bpr_loss, PairwiseInteractions)
 import utils
-
+import config as myconfig
 
 def explicit():
     print ("inside explicit")
@@ -16,7 +17,6 @@ def explicit():
                             lr=0.02, weight_decay=0.1,
                             optimizer=torch.optim.Adam, n_epochs=4,
                             verbose=True, random_seed=2017)
-    pipeline.fit()
     return pipeline
 
 
@@ -33,7 +33,6 @@ def implicit():
                            interaction_class=PairwiseInteractions,
                            eval_metrics=('auc', 'patk'))
     print ("before fit")
-    pipeline.fit()
     return pipeline
 
 
@@ -49,39 +48,57 @@ def hogwild():
                             model=BPRModule, hogwild=True,
                             interaction_class=PairwiseInteractions,
                             eval_metrics=('auc', 'patk'))
+    return pipeline
+
+def trainModel(config):
+    train, test = utils.get_movielens_train_test_split(implicit=True)
+
+    # print((test))
+
+    pipeline = explicit()
+    # model = implicit()
+    # model = hogwild()
     pipeline.fit()
+
+    checkpoint_file = os.path.join(config.save_dir, "checkpoint.pth")
+
+    user, item = utils.getUserItem()
+    torch.save({
+        "model": pipeline.model.state_dict(),
+        "optimizer": pipeline.optimizer.state_dict(),
+    }, checkpoint_file)
+
+
+def testModel(config):
+    bestmodel_file = os.path.join(config.save_dir, "checkpoint.pth")
+    load_res = torch.load(bestmodel_file, map_location="cpu")
+    pipeline = explicit()
+    # model = implicit()
+    # model = hogwild()
+    pipeline.model.load_state_dict(load_res["model"])
+    pipeline.optimizer.load_state_dict(load_res["optimizer"])
+    pipeline.model.eval()
+
+    testdata=np.zeros(3,3)
+
+    result=pipeline.model.forward(testdata)
 
 
 if __name__ == '__main__':  
-    train, test = utils.get_movielens_train_test_split(implicit=True)
-    print ((test))
-    #print (test).shape[0]
-    parser = argparse.ArgumentParser(description='torchmf')
-    parser.add_argument('--example',
-                        help='explicit, implicit, or hogwild')
-    args = parser.parse_args()
-    #args ='implicit'
-    #implicit()
-    if args.example == 'explicit':
-        model = explicit()
-    elif args.example == 'implicit':
-        model = implicit()
-    elif args.example == 'hogwild':
-        model = hogwild()
+
+    config, unparsed = myconfig.get_config()
+    # ----------------------------------------
+    # Parse configuration
+    # If we have unparsed arguments, print usage and exit
+    if len(unparsed) > 0:
+        print("unparsed for DQN :", unparsed)
+        # input("Press Enter to continue...")
+
+    if config.mode == "train":
+        trainModel(config)
+    elif config.mode == "test":
+        testModel(config)
     else:
-        print('example must be explicit, implicit, or hogwild')
-        
-        
-    user ,item =utils.getUserItem()
-    torch.save({
-                "model": model.model.state_dict(),
-                "optimizer": model.optimizer.state_dict(),
-            },'/Users/rahul.singh/Code/modelSave1.pth')
-    
-    #model.model.eval();
-    #model.model.predict([2],[2])
-    
-    #model.model.state_dict
-    #print(test.describe())
-    #print(model.model.predict())
+        raise ValueError("Unknown run mode \"{}\"".format(config.mode))
+
     
